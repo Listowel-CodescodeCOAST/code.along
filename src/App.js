08ac240ca35c19afebc4898344815b54c6b27a97
Listowel-CodescodeCOAST <adusarkodielistowel@yahoo.com>
2022-03-23 13:59:07 +0000

    React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
-
-import writers from './writers';
+// import './App.css';
+import { useEffect, useState } from 'react';
+// import writers from './writers';
 import { ProfileCard } from './ProfileCard';
 
 function App() {
+  const [writers, setWriter] = useState([]);
+
+  useEffect(() => {
+    const getWriters =async () => {
+      const response = await fetch("/writers.json");
+      const data = await response.json();
+      setWriter(data);
+    };
+    getWriters();
+  }, []);
   return (
     <div>
       <h1>Writer Profiles</h1>
