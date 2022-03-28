1b0c3e9df583d718bed033fa2b963da0df52cc4c
Listowel-CodescodeCOAST <adusarkodielistowel@yahoo.com>
2022-03-28 14:46:52 +0000

    react-component5

--- src/App.js
+++ src/App.js
@@ -1,61 +1,31 @@
 import { useEffect, useState } from 'react';
+import ProfileForm from './component/ProfileForm';
 // import writers from './writers';
 import ProfileCard from './ProfileCard';
 
 function App() {
-  const [data, setData] = useState({
-    writers: [],
-    loading: false,
-  });
-
-  const handleClick =() => {
-    setData(prevData => ({
-      ...prevData,
-      loading: true,
-    }));
-
-
-    setTimeout(() => {
-       const getWriters =async () => {
-         const response = await fetch("/writers.json");
-         const data = await response.json();
-         setData({
-           writers: data,
-           loading: false,
-         });
-       };
-       getWriters();
-     }, 2000);
-  };
-
-    if(data.loading) {
-      return (
-        <div>
-          <h1>Writers Profile</h1>
-          <div className='Container'>
-            <div className='card action'>  
-              <p className='infoText'> Loading...</p>
-            </div>
-          </div>
-        </div>
-      );
-    }
+  const [allProfile, setAllprofile] = useState([
+    {
+      firstName: "Listowel",
+      lastName: "ADU",
+      email: "listowe.adu.com",
+      phone: "+200 000 123 012",
+    },
+  ]);
+  const submit = (profile) => {
+    const arr = allProfile;
+    arr.push(profile);
+    setAllprofile(arr);
+  }
 
   return (
     <div>
       <h1>Writer Profiles</h1>
       <div className="contaner">
-        {data.writers.length === 0 ? (
-          <div className="card action"> 
-          <p className='infoText'> Oops... no writer profile found</p>
-          <button className='actionBtn' onClick={handleClick}>
-            Get Writers
-          </button>
-          </div>
-        ) : (
-        data.writers.map((writer) => (
-          <ProfileCard key={writer.id} writer={writer}/>
-        )))}
+        <ProfileForm submit={submit}/>
+        {allProfile.map((writer) => (
+          <ProfileCard key={writer.id} writer={writer} />
+        ))}
       </div>
     </div>
   );
