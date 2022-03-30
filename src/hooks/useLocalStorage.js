6865370fd2e7017e4131a926800a334093790cbe
Listowel-CodescodeCOAST <adusarkodielistowel@yahoo.com>
2022-03-30 14:26:41 +0000

    React Component-7

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const useLocalStorage = (key, initilValue) => {
+    const [value, setValue] = useState(() => {
+        if (typeof window === undefined){
+           return initilValue;
+       }
+   try {
+       const data = window.localStorage.getItem(key);
+       return data ? JSON.parse(data) : initilValue;
+   } catch (error) {
+       console.log(error);
+       return initilValue;
+   }
+})                                                                                                                                                                     
+
+const setLocalStorage =(value) => {
+    try {
+        setValue(value)
+
+        if (typeof window !== undefined) {
+            window.localStorage.setItem(key, JSON.stringify(value));
+        }
+    } catch (error) {
+
+    }
+}
+
+  return [value, useLocalStorage];
+};
+
+export default useLocalStorage
